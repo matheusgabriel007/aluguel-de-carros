03ad11ca3a1761b2c58875433a316fcd3298c850
matheusgabriel007 <matheusgabriel007>
2018-04-10 21:21:59 -0300

    primeiro commit

---
+++ login.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Honda - Promoções</title>
+	<meta charset="utf-8" lang="pt-br">
+	
+
+	<!--FAV ICON-->
+	<link rel="shortcut icon" href="imagens/fav-icon.png"/>
+
+	<!--CSS-->
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+	<link rel="stylesheet" type="text/css" href="css/promocoes.css">
+
+	<!--FONTE ICONE-->
+	<link rel="stylesheet" type="text/css" href="fontes/font-icon.css">
+
+	<!-- STYLE SLIDE -->
+	<link rel="stylesheet" type="text/css" href="slide/engine1/style.css" />
+	<!-- FIM STYLE SLIDE -->
+
+	<!-- SCRIPT SLIDE -->
+	<script type="text/javascript" src="slide/engine1/jquery.js"></script>
+	<!-- FIM SCRIPT SLIDE -->
+
+	<!-- STYLE ALERT -->
+	<link rel="stylesheet" type="text/css" href="alertfy/themes/alertify.core.css" />
+	<link rel="stylesheet" type="text/css" href="alertfy/themes/alertify.default.css" />
+	<!-- FIM ALERT SLIDE -->
+
+</head>
+
+<body>
+	<header id="cabecalho">
+
+		<div id="divisoria1"></div>
+
+		<section id="divisoriaContato">
+			<div id="englobaDivisoriaContato">
+				<div id="redesSociais">
+					<a href="http://www.facebook.com.br" target="_blank"><h1 title="Curta nossa página no Facebook!" class="icon-facebook-with-circle tamanhoRedeSocialTop"></span></h1></a> <!--FIM FACEBOOK-->
+
+					<a href="https://www.instagram.com/?hl=pt-br" target="_blank"><h1 title="Siga-nos no Instagram!" class="icon-instagram-with-circle tamanhoRedeSocialTop"></span></h1></a> <!--FIM INSTAGRAM-->
+
+					<a href="http://www.youtube.com.br" target="_blank"><h1 title="Acompanhe nosso Canal!" class="icon-youtube-with-circle tamanhoRedeSocialTop"></span></h1></a> <!--FIM YOUTUBE-->
+
+					<a href="https://play.google.com/store/apps/details?id=com.whatsapp&hl=pt" target="_blank"><h1 title="+55 21 90000-0000" class="icon-whatsapp tamanhoRedeSocialTop"></span></h1></a> <!--FIM WHATSAPP-->
+				</div> <!--FIM REDES SOCIAIS-->
+
+
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+				<p class="contatoTop"><span class="icon-phone">    </span>+55 21 0000-0000</p> <!--FIM TELEFONE-->
+			</div>
+		</section> <!--FIM DIVISORIA CONTATO-->
+
+		<section class="divisoria2Cabecalho">
+			<div id="englobaDivisoria2Cabecalho">
+
+				<a href="index.html"><img id="logo" src="imagens/logohonda1.png" width="150px"></a> <!--FIM LOGO-->
+
+				<div id="englobaLoginInput">
+					
+					<input type="text" placeholder="Pesquisar ..." class="inputTop" name="inputTopName">
+				</div>
+
+				<nav id="menu">
+					<ul>
+						<a href="index.html"><li class="liMenu">Home</li></a>
+						<a href="sobre.html"><li class="liMenu">Sobre</li></a>
+						<a href="promocoes.html"><li style="color:#e97027;" class="liMenu">Promoções</li></a>
+						<a href="fazer-pedido.html"><li class="liMenu">Fazer Pedido</li></a>
+						<a href="contato.html"><li class="liMenu">Contato</li></a>
+					</ul>
+				</nav>  <!--FIM MENU-->
+			</div>
+		</section>
+<p></p> <p></p>
+<p></p>
+
+<form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-group form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-dark">Submit</button>
+</form>
+
+	</header> <!--FIM CABECALHO-->
+<p></p>
+	
+
+
+	<article id="corpo">
+
+
+	</article> <!--FIM CORPO-->
+
+	<footer class="rodape">
+		<div id="redesSociaisFooter">
+			<div id="englobaSocialFooter">
+				<div id="voltaInicio">
+					<a class="scroll" href="#cabecalho"><h1 title="Voltar para o início!" class="icon-circle-up tamanhoRedeSocialTop"></span></h1></a> <!--FIM VOLTA INICIO-->
+				</div>
+		
+		</div>
+		<div id="direitosLinhaCima"></div>
+		<div id="direitosReservados">
+			<p align="center">© Copyright 2017 - Honda - Todos os direitos reservados</p>
+		</div>
+	</footer> <!--FIM RODAPÉ-->
+
+	<!--JQUERY-->
+	<script type="text/javascript" src="jquery/jquery-3.1.1.min.js"></script>
+	<script type="text/javascript" src="jquery/funcoes-jquery.js"></script>
+	
+	<!--JAVASCRIPT-->
+	<script type="text/javascript" src="javascript/funcoes-javascript.js"></script>
+	
+	<!-- SCRIPT ALERT -->
+	<script type="text/javascript" src="alertfy/js/alertify.min.js"></script>
+	<!-- FIM SCRIPT ALERT -->
+
+</body>
+</html>
